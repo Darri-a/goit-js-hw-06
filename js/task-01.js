90acf99f68f247d9dfad6abde4c7e67afbc822dc
Daria <sorokinadasha3628@gmail.com>
2023-08-30 19:59:37 +0200

    1,2 ex

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,21 @@
 const categoriesItem = document.querySelectorAll(".item");
 
+console.log(`Number of categories: ${categoriesItem.length}`);
+
 categoriesItem.forEach((element) => {
   const categoryTitle = element.querySelector("h2").textContent;
+  const elementsCount = element.querySelectorAll("li").length;
   console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${elementsCount}`);
 });
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+let ingredientsList = "";
+
+for (let i = 0; i < ingredients.length; i++) {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.className = "item";
+  ingredientItem.textContent = ingredients[i];
+  ingredientsList += ingredientItem.outerHTML;
+}
+
+document.getElementById("ingredients").innerHTML = ingredientsList;
